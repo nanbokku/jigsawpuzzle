554fa86a50d977a1d5144d7dd9a5617f1bbe5511
nanbokku <n1510126@edu.cc.uec.ac.jp>
2018-11-11 12:50:18 +0900

    [add] Add imgui library.

--- JigsawPuzzle/src/main.cpp
+++ JigsawPuzzle/src/main.cpp
@@ -1,13 +1,69 @@
 #include "PieceCreater.h"
 #include <opencv2/opencv.hpp>
 #include <GL/glut.h>
+#include "imgui.h"
+#include "imgui_impl_freeglut.h"
+#include "imgui_impl_opengl2.h"
 
 using namespace cv;
 using namespace std;
 
+bool show_demo_window = false;
+bool show_another_window = true;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+void gui()
+{
+	//if (show_demo_window) {
+	//	ImGui::ShowDemoWindow(&show_demo_window);
+	//}
+
+	static float f = 0.0f;
+	static int counter = 0;
+
+	ImGui::Begin("Piece Box");
+	
+	ImGui::End();
+
+	ImGui::Begin("Hello world.");
+	//ImGui::Checkbox("Demo Window", &show_demo_window);
+	ImGui::Checkbox("Another Window", &show_another_window);
+
+	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+	ImGui::ColorEdit3("clear color", (float*)&clear_color);
+
+	if (ImGui::Button("Button")) counter++;
+	ImGui::SameLine();
+	ImGui::Text("counter = %d", counter);
+
+	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	ImGui::End();
+
+	if (show_another_window) {
+		ImGui::Begin("Another Window", &show_another_window);
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me")) show_another_window = false;
+		ImGui::End();
+	}
+}
+
 void display()
 {
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplFreeGLUT_NewFrame();
+
+	gui();
+
+	ImGui::Render();
+
+	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+	glutSwapBuffers();
+	glutPostRedisplay();
+
 	glFlush();
 }
 
@@ -33,12 +89,27 @@ int main(int argc, char* argv[])
 
 	glutInitWindowSize(800, 600);
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
 	glutCreateWindow(argv[0]);
+
 	glutDisplayFunc(display);
 	init();
 	glutReshapeFunc(resize);
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	ImGui_ImplFreeGLUT_Init();
+	ImGui_ImplFreeGLUT_InstallFuncs();
+	ImGui_ImplOpenGL2_Init();
+
+	ImGui::StyleColorsDark();
+
 	glutMainLoop();
+
+	ImGui_ImplOpenGL2_Shutdown();
+	ImGui_ImplFreeGLUT_Shutdown();
+	ImGui::DestroyContext();
 
 	return 0;
 }
